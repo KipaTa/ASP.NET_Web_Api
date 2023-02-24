eb99da5a6195827709a89ea9aed0bf526681d48d
KipaTa <kirsi.tainio@gmail.com>
2023-02-24 15:43:40 +0200

    feat: Migrations, DBContext added

---
+++ Models/MovieCharactersDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MovieCharactersAPI.Models
+{
+    public class MovieCharactersDbContext: DbContext
+    {
+        public DbSet<Movie> Movies { get; set;}
+        public DbSet<Character> Characters { get; set;}
+        public DbSet<Franchise> Franchises { get;set;}
+
+        public MovieCharactersDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
