9fb31f92aada4f40f1953b05f3376aa0cf4211bf
KipaTa <kirsi.tainio@gmail.com>
2023-02-24 16:16:24 +0200

    Feat: Database updated

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public string Genre { get; set; }
-        public string Year { get; set; }
+        public int Year { get; set; }
 
         public string Director { get; set; }
 
