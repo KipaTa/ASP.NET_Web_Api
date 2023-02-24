9696286ff823a6574aa3c751fc155759f4524de7
KipaTa <kirsi.tainio@gmail.com>
2023-02-24 15:19:23 +0200

    Add project files.

---
+++ Models/Character.cs
@@ -0,0 +1,14 @@
+﻿namespace MovieCharactersAPI.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Alias { get; set; }
+
+        public string Gender { get; set; }
+        public string Picture { get; set; }
+
+        public ICollection<Movie> Movies { get; set; }
+    }
+}
