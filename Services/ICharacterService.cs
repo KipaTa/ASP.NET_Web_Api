8d855160a36876cdfefcce21bf062065bb4014cd
Heidi Joensuu <heidi.joensuu@fi.experis.com>
2023-02-28 10:18:07 +0200

    implimented services and controllers

---
+++ Services/ICharacterService.cs
@@ -0,0 +1,13 @@
+﻿using MovieCharactersAPI.Models;
+
+namespace CharacterCharactersAPI.Services
+{
+    public interface ICharacterService
+    {
+        Task<IEnumerable<Character>> GetAllCharacters();
+        Task<Character> GetCharacterById(int id);
+        Task<Character> AddCharacter(Character character);
+        Task DeleteCharacter(int id);
+        Task<Character> UpdateCharacter(Character character);
+    }
+}
---
+++ Services/IFranchiseService.cs
@@ -0,0 +1,14 @@
+﻿
+using MovieCharactersAPI.Models;
+
+namespace MovieCharactersAPI.Services
+{
+    public interface IFranchiseService
+    {
+        Task<IEnumerable<Franchise>> GetAllFranchises();
+        Task<Franchise> GetFranchiseById(int id);
+        Task<Franchise> AddFranchise(Franchise franchise);
+        Task DeleteFranchise(int id);
+        Task<Franchise> UpdateFranchise(Franchise franchise);
+    }
+}
